3344dfae039c04a8693bfa11800c78978a78d2b7
Mateus Araujo <mateuscruz22@gmail.com>
2019-05-08 21:07:46 -0300

    Add document directory

---
+++ docs/jcarstore.sql
@@ -0,0 +1,113 @@
+-- MySQL dump 10.13  Distrib 8.0.16, for Win64 (x86_64)
+--
+-- Host: localhost    Database: jcarstore
+-- ------------------------------------------------------
+-- Server version	8.0.16
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `administrador`
+--
+
+DROP TABLE IF EXISTS `administrador`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `administrador` (
+  `id_administador` int(11) NOT NULL,
+  `nome_administrador` varchar(255) DEFAULT NULL,
+  `email_administrador` varchar(255) NOT NULL,
+  `senha_administrador` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_administador`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `cliente` (
+  `id_cliente` int(11) NOT NULL,
+  `cpf_cliente` int(11) NOT NULL,
+  `nome_cliente` varchar(255) NOT NULL,
+  `nascimento_cliente` date DEFAULT NULL,
+  `foto_cliente` varchar(255) DEFAULT NULL,
+  `endereco_cliente` varchar(1000) DEFAULT NULL,
+  `email_cliente` varchar(255) NOT NULL,
+  `senha_cliente` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_cliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `veiculo`
+--
+
+DROP TABLE IF EXISTS `veiculo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `veiculo` (
+  `id_veiculo` int(11) NOT NULL,
+  `modelo_veiculo` varchar(255) NOT NULL,
+  `marca_veiculo` varchar(255) DEFAULT NULL,
+  `ano_veiculo` int(11) DEFAULT NULL,
+  `foto_veiculo` varchar(255) DEFAULT NULL,
+  `descricao_veiculo` varchar(1000) DEFAULT NULL,
+  `preco_custo` double NOT NULL,
+  `preco_venda` double NOT NULL,
+  PRIMARY KEY (`id_veiculo`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `venda`
+--
+
+DROP TABLE IF EXISTS `venda`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `venda` (
+  `id_venda` int(11) NOT NULL,
+  `id_cliente` int(11) NOT NULL,
+  `id_veiculo` int(11) NOT NULL,
+  `quantidade_comprada` int(11) NOT NULL,
+  `lucro_venda` float NOT NULL,
+  `data_venda` date NOT NULL,
+  PRIMARY KEY (`id_venda`),
+  KEY `venda_fk_cliente` (`id_cliente`),
+  KEY `venda_fk_veiculo` (`id_veiculo`),
+  CONSTRAINT `venda_fk_cliente` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id_cliente`),
+  CONSTRAINT `venda_fk_veiculo` FOREIGN KEY (`id_veiculo`) REFERENCES `veiculo` (`id_veiculo`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping events for database 'jcarstore'
+--
+
+--
+-- Dumping routines for database 'jcarstore'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-05-08 20:57:05
